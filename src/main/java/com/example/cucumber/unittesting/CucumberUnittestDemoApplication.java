76cc0ae3954d2fa9568e1c8268e272d11b524287
TreasureMa <t.mathala@gmail.com>
2019-06-06 20:14:41 +0200

    Initial commit

---
+++ src/main/java/com/example/cucumber/unittesting/CucumberUnittestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cucumber.unittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberUnittestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberUnittestDemoApplication.class, args);
+	}
+
+}
